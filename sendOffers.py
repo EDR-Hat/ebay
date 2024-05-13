757c929735373b585785076667e8ec3fbfd7303b
EDR-Hat <aztecev2012@gmail.com>
2024-05-13 11:37:39 -0700

    Update sendOffers.py
    updated offers to be closer to how they are being used in production. will add later

--- sendOffers.py
+++ sendOffers.py
@@ -1,6 +1,3 @@
-#import psycopg2 as psy
-import pandas as pd
-#import os
 import math
 import time
 import datetime
@@ -13,21 +10,6 @@ s = time.time()
 def truncate(x):
     return x - x % 0.01
 
-#first draft won't use the database
-#with open('db.cred', 'r') as f:
-#    cred = json.load(f)
-#f.close()
-
-#conn = psy.connect(
-#        host='localhost',
-#        database="postgres",
-#        user="postgres",
-#        password=cred['secret']
-#        )
-
-#cur = conn.cursor()
-
-offerController = pd.read_csv('/home/magicmoney/ebay/directives/offers.csv')
 endpoint = 'https://api.ebay.com/sell/negotiation/v1/find_eligible_items'
 r = makeRequest('get', endpoint, {  'X-EBAY-C-MARKETPLACE-ID': 'EBAY_US'}, parameters={'limit': 200})
 
@@ -65,33 +47,9 @@ for itm in itms:
         print("price too low for an offer")
         continue
 
-    default = offerController[offerController['category'] == 'default'].loc[0]
-    delay = (datetime.datetime.now(datetime.timezone.utc) - startTime).days
-    aggPercent = default['dropPercentage'] * math.floor((delay - default['delay'])/ default['timeInterval'])
-    if aggPercent < 0:
-        aggPercent = 0
-    offer1 = default['initial'] + aggPercent
-    if offer1 >= 0.5:
-        offer1 = 0.5
-    
-    offer1 = truncate( price - (price * offer1))
-    diff = 1.0 - (offer1/oldPrice)
-    if diff < 0.05:
-        offer1 = truncate(price - (0.05 * price))
-    offer2 = truncate( price - (price * default['maxOfferPercentage']))
-    finalOffer = max(offer1, offer2)
-    if finalOffer < 0.99:
-        print('price too low to apply time based discount')
-        print('price', price)
-        print('offer1', offer1)
-        print('offer2', offer2)
-        print('finalOffer', finalOffer)
-        print('itm num', itm)
-        continue
-
     load = {}
     #load['message'] = 'ebay suggests giving a customized message to each offer, apparrently it increases conversion. this might be worth doing some sort of formulaic one that is different than the ebay default'
-    load['offeredItems'] = [ {'listingId': itm, 'price': { 'currency': 'USD', 'value': str(finalOffer)}, 'quantity': 1} ]
+    load['offeredItems'] = [ {'listingId': itm, 'discountPercentage': "5", 'quantity': 1} ]
     load = json.dumps(load)
 
     endpoint = 'https://api.ebay.com/sell/negotiation/v1/send_offer_to_interested_buyers'
@@ -112,16 +70,3 @@ for itm in itms:
 
 e = time.time()
 print(e - s)
-    
-
-
-
-
-
-
-
-#future idea is to simply grab data about the item from the database
-#for now it uses data that's retrived from the api
-#for each item id, follow the offer directive based on its settings
-# -this might mean grabbing the price and time data for that item in the local database
-# -basic directive should take into account time first listed, current price and initial price
